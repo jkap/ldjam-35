3b7f87a95bc0c330b41753e01b7c9f954ab8495b
Jenn Kaplan <me@jkap.io>
2016-04-10 15:33:40 -0700

    Changed image to relative paths for gh-pages

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { Phaser } from 'phaser';
 
 const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
   preload: () => {
-    game.load.image('logo', '/images/phaser.png');
+    game.load.image('logo', 'images/phaser.png');
   }, create: () => {
     const logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
