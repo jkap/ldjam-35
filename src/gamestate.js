29cb141c120e9c7661a0ae0a97f3292d7054aa3a
Jessica Haylay <hey@jayhay.me>
2016-04-16 18:14:47 -0400

    DESTROY THE FRIENDS YOU MADE ALONG THE WAY

--- src/gamestate.js
+++ src/gamestate.js
@@ -3,17 +3,12 @@
 import { Phaser } from 'phaser';
 import { TrackManager } from './track-manager';
 
-import { HeadSprite, PickUpFriendSprite } from 'sprites';
 import { Grid } from 'grid';
 
 import * as timeUtil from './time-util';
 
 class GameState extends Phaser.State {
   preload() {
-    this.load.image('circle', 'images/circle.png');
-    this.load.image('triangle', 'images/triangle.png');
-    this.load.image('square', 'images/square.png');
-    this.load.image('star', 'images/star.png');
     TrackManager.getTrack('track-1', this)
       .then(track => {
         this.track = track;
@@ -21,15 +16,12 @@ class GameState extends Phaser.State {
   }
 
   create() {
-    this.grid = new Grid(20, 20, 20, 10);
+    this.grid = new Grid(3, 7, 100, 10);
     const gridSize = this.grid.getSize();
     this.game.scale.setGameSize(gridSize.width, gridSize.height);
-    this.headSprite = new HeadSprite(this.game, this.grid, 10, 10);
 
-    // Find your friend <3
-    this.curFriend = null;
-
-    this.spawnFriend();
+    this.graphics = this.game.add.graphics(0, 0);
+    window.graphics = this.graphics;
   }
 
   update() {
@@ -45,32 +37,21 @@ class GameState extends Phaser.State {
   }
 
   render() {
+    this.graphics.clear();
 
+    // Draw the grid in the background
+    this.graphics.beginFill(0x1c1c1c);
+    for (let x = 0; x < this.grid.width; x++) {
+      for (let y = 0; y < this.grid.height; y++) {
+        const pos = this.grid.gridToPixelPos({ x: x, y: y });
+        this.graphics.drawRect(pos.x - this.grid.squareSize / 2, pos.y - this.grid.squareSize / 2,
+                               this.grid.squareSize, this.grid.squareSize);
+      }
+    }
+    this.graphics.endFill();
   }
 
   advance() {
-    this.headSprite.advance();
-    if (this.curFriend) {
-      if (this.headSprite.gridPos.x === this.curFriend.gridPos.x
-       && this.headSprite.gridPos.y === this.curFriend.gridPos.y) {
-        this.headSprite.pickUp(this.curFriend.shape);
-        this.spawnFriend();
-      }
-    }
-  }
-
-  spawnFriend() {
-    const sx = this.game.rnd.integerInRange(0, this.grid.width - 1);
-    const sy = this.game.rnd.integerInRange(0, this.grid.height - 1);
-
-    if (!this.curFriend) {
-      this.curFriend = new PickUpFriendSprite(this.game, this.grid, sx, sy);
-    } else {
-      this.curFriend.gridPos.x = sx;
-      this.curFriend.gridPos.y = sy;
-      this.curFriend.setPos();
-      this.curFriend.pickShape();
-    }
   }
 
   playTrack(track) {
